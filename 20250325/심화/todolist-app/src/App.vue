fc58d470aa598b7b09e6a08da704f1612fb2808e
Jiho <rkdwlgh01@naver.com>
2025-03-24 17:32:09 +0900

    20250325 과제 - 심화 문제

---
+++ 20250325/심화/todolist-app/src/App.vue
@@ -0,0 +1,95 @@
+<!-- vueinit : vue3 기본 형태 스니펫(자동 완성) -->
+
+<template>
+  <div id="app" class="container">
+    <div class="card card-body bg-light">
+      <div class="title">:: Todolist App</div>
+    </div>
+    <div class="card card-default card-borderless">
+      <div class="card-body">
+        <!-- 입력용 컴포넌트 방출(add-todo)된 이벤트가 감지 되었을 때 
+          addTodo 메서드 호출-->
+        <InputTodo @add-todo="addTodo" />
+
+        <!-- 카운트 컴포넌트 -->
+        <TodoCount v-bind:todolist="todolist" />
+
+        <!-- 목록 컴포넌트 
+          - 목록 화면 렌더링에 필요한 데이터 todolist를 
+          부모(App.vue)가 가지고 있음
+          - 부모 -> 자식 데이터 전달 : props 이용
+            -> v-bind: 전달할 이름="데이터"
+        -->
+        <TodoList
+          v-bind:todolist="sortedTodolist"
+          @toggle-completed="toggleCompleted"
+          @delete-todo="deleteTodo"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+// import 태그명 from '경로';
+import InputTodo from './components/InputTodo.vue';
+import TodoList from './components/TodoList.vue';
+import TodoCount from './components/TodoCount.vue';
+
+export default {
+  // 사용할 자식 컴포넌트 등록
+  components: { InputTodo, TodoList, TodoCount },
+
+  data() {
+    return {
+      todolist: [
+        { id: Date.now(), todo: '자전거 타기', completed: false },
+        { id: Date.now() + 1, todo: '딸과 공원 산책', completed: true },
+        { id: Date.now() + 2, todo: '일요일 애견 카페', completed: false },
+        { id: Date.now() + 3, todo: 'Vue 원고 집필', completed: false },
+      ],
+    };
+  },
+  methods: {
+    // receivwTodo : InputTodo 컴포넌트의
+    // this.$emit('add-todo', this.todo); 에서 전달된 this.todo(payload)
+    addTodo(receiveTodo) {
+      const td = {
+        id: Date.now(),
+        todo: receiveTodo,
+        completed: false,
+      };
+      this.todolist.push(td);
+    },
+
+    // 매개변수 id : TodoListItem 에서 부터 전달된 id
+    toggleCompleted(id) {
+      let index = this.todolist.findIndex((item) => id === item.id);
+      this.todolist[index].completed = !this.todolist[index].completed;
+    },
+
+    deleteTodo(id) {
+      let index = this.todolist.findIndex((item) => id === item.id);
+      this.todolist.splice(index, 1);
+    },
+  },
+
+  // computed : data가 변할 때 자동으로 계산
+  // -> 계산되어 return된 값은 함수명(sortedList)으로 사용
+  computed: {
+    sortedTodolist() {
+      this.todolist.sort((todo1, todo2) => {
+        if (
+          (todo1.completed && todo2.completed) ||
+          (!todo1.completed && !todo2.completed)
+        ) {
+          if (todo1.todo.toLowerCase() > todo2.todo.toLowerCase()) return 1;
+        } else if (todo1.completed || todo2.completed) {
+          if (todo1.completed) return 1;
+        }
+        return -1;
+      });
+      return this.todolist;
+    },
+  },
+};
+</script>
---
+++ 20250325/심화/todolist-app/src/components/TodoCount.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="d-flex justify-content-around mb-3">
+    <div>전체 todo 개수: {{ totalTodo }}</div>
+    <div>완료 todo 개수: {{ completedTodo }}</div>
+    <div>미완료 todo 개수: {{ uncompletedTodo }}</div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'TodoCount',
+  // 부모로 부터 데이터 전달 받기 + 유효성 검사
+  props: {
+    todolist: { type: Array, required: true },
+  },
+  computed: {
+    completedTodo() {
+      return this.todolist.filter((todo) => todo.completed).length;
+    },
+    totalTodo() {
+      return this.todolist.length;
+    },
+    uncompletedTodo() {
+      return this.todolist.filter((todo) => !todo.completed).length;
+    },
+  },
+};
+</script>
