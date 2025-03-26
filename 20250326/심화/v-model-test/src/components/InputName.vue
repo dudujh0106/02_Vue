d9d99ac62e4181a2e3a14217901457fa3b5b2ba0
Jiho <rkdwlgh01@naver.com>
2025-03-26 16:32:53 +0900

    20250326 과제 - 심화 문제

---
+++ 20250326/심화/v-model-test/src/components/InputName.vue
@@ -0,0 +1,19 @@
+<template>
+  <input
+    type="text"
+    :value="name"
+    @input="$emit('update:name', $event.target.value)"
+  />
+</template>
+
+<script>
+export default {
+  name: 'InputName',
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
