eacaf12169d33fd87df221d8171fbcfd6a8f8600
Jiho <rkdwlgh01@naver.com>
2025-03-27 15:06:40 +0900

    20250327 과제 - 기본 문제

---
+++ 20250327/기본/calc-component-test/src/components/Calc5.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    x : <input type="text" v-model.number="x" /><br />
+    <div>결과 : {{ result }}</div>
+  </div>
+</template>
+
+<script>
+import { ref, watch } from 'vue';
+
+export default {
+  name: 'Calc5',
+  setup() {
+    const x = ref(0);
+    const result = ref(0);
+
+    watch(x, (newValue, oldValue) => {
+      console.log(`x의 새로운 값: ${newValue} 이전 값: ${oldValue}`);
+    });
+
+    return { x, result };
+  },
+};
+</script>
