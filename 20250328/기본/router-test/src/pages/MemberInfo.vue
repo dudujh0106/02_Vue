84d734e17e8285864d1cfc6392257125922e617c
Jiho <rkdwlgh01@naver.com>
2025-03-28 16:33:25 +0900

    20250328 과제 - 기본 문제

---
+++ 20250328/기본/router-test/src/pages/MemberInfo.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="card card-body">
+    <!-- <h2>Member Info</h2>
+    <div>
+      경로 패턴: /members/:id<br />
+      요청 경로: {{ currentRoute.path }}<br />
+      id값: {{ currentRoute.params.id }}<br />
+    </div> -->
+    <div class="mt-5">
+      <img :src="member.photo" alt="멤버 사진" />
+      <div>{{ member.name }}({{ member.role }})</div>
+      <div>{{ member.desc }}</div>
+      <div class="my-3">
+        <a
+          v-if="member.insta && member.insta != ''"
+          class="fa fa-instagram"
+          :href="member.insta"
+        ></a>
+        <a
+          v-if="member.facebook && member.facebook != ''"
+          class="fa fa-facebook"
+          :href="member.facebook"
+        ></a>
+        <a
+          v-if="member.youtube && member.youtube != ''"
+          class="fa fa-youtube"
+          :href="member.youtube"
+        ></a>
+      </div>
+      <router-link to="/members">멤버 목록으로</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+import { useRoute } from 'vue-router';
+import members from '@/members.json';
+
+export default {
+  name: 'MemberInfo',
+
+  setup() {
+    // useRoute() : 현재 라우트 정보 객체
+    const currentRoute = useRoute();
+    const id = parseInt(currentRoute.params.id);
+
+    const member = members.find((mem) => mem.id === id);
+
+    console.log('currentRoute :', currentRoute);
+
+    return { currentRoute, member };
+  },
+};
+</script>
---
+++ 20250328/기본/router-test/src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import Home from '@/pages/Home.vue';
+import About from '@/pages/About.vue';
+import Members from '@/pages/Members.vue';
+import Videos from '@/pages/Videos.vue';
+import MemberInfo from '@/pages/MemberInfo.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: Home },
+    { path: '/about', component: About },
+    { path: '/members', component: Members },
+    { path: '/videos', component: Videos, name: 'vi' },
+
+    /* 동적 라우트 */
+    { path: '/members/:id', component: MemberInfo },
+  ],
+});
+
+export default router;
