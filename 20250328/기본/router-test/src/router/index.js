b7087045d98657b15bc091f3a87f7126f36ee430
Jiho <rkdwlgh01@naver.com>
2025-03-28 17:26:57 +0900

    20250328 과제 - 심화 문제

--- 20250328/기본/router-test/src/router/index.js
+++ 20250328/기본/router-test/src/router/index.js
@@ -5,17 +5,30 @@ import About from '@/pages/About.vue';
 import Members from '@/pages/Members.vue';
 import Videos from '@/pages/Videos.vue';
 import MemberInfo from '@/pages/MemberInfo.vue';
+import NotFound from '@/pages/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
-    { path: '/members', component: Members },
+    { path: '/', name: 'home', component: Home },
+    { path: '/about', name: 'about', component: About },
+    { path: '/members', name: 'members', component: Members },
     { path: '/videos', component: Videos, name: 'vi' },
 
     /* 동적 라우트 */
-    { path: '/members/:id', component: MemberInfo },
+    {
+      path: '/members/:id',
+      name: 'members/id',
+      component: MemberInfo,
+      beforeEnter: (to, from) => {
+        // members/:id 경로는 /members에서만 접근 가능
+        if (from.name !== 'members' && from.name !== 'members/id') {
+          return false;
+        }
+      },
+    },
+
+    { path: '/:paths(.*)*', name: 'NotFound', component: NotFound },
   ],
 });
 
